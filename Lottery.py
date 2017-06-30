4bfb45feb75e1ab880edb4e1ee88ef6ba8dabb13
JackelenR <jackelenr@Macs-MacBook-Pro-99.local>
2017-06-30 11:36:24 -0700

    donations

--- Lottery.py
+++ Lottery.py
@@ -15,9 +15,18 @@ elif vote == "yes":
     print("20x chance of homocide")
     print("And HIGHER CHANCES OF:\ndrug overdose\nbankruptcy\nkidnapping")
 
-    money = (input("Do you want to take it all out as cash or slowly over time? (cash or time)\n")
+    money = (input("Do you want to take it all out as cash or slowly over time? (cash or time)\n"))
 
-    elif money == "time":
+    if money == "time":
         print("You just lost $$ due to annual taxes! - Bankrupt!")
     elif money == "cash":
         print("Good! You managed to avoid losing money")
+        print("Someone begged you to donate money")
+
+        donate = (input("Will you donate? (yes or no)\n"))
+
+        if donate == "yes":
+            print("Bankrupt! After you said yes, everyone swarmed you and took your money")
+        elif donate == "no":
+            print("Great! You avoided going bankrupt due to people swarming you after the donation was given.")
+        
