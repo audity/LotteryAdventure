00121875f16cc05fe87ca5f31745ef67707d3182
AudreyS <audreys@Macs-MacBook-Pro-99.local>
2017-06-30 14:18:30 -0700

    added loop, ctrl c to exit

--- Lottery.py
+++ Lottery.py
@@ -1,87 +1,91 @@
 # Text Adventure - start
 
-print("Theres a lottery!")
-print("It says:\nLocal Buster Lottery! 600 Million Dollars is on the line!")
+go_again = True
 
-# Cast your vote
+while go_again == True:
+    print("Theres a lottery!")
+    print("It says:\nLocal Buster Lottery! 600 Million Dollars is on the line!")
 
-vote = (input("Cast your vote? (yes or no)\n"))
+    # Cast your vote
 
-if vote == "no":
-    print("k. bye.")
-elif vote == "yes":
-    print("You have won 600 million dollars!")
-    print("You have recieved powerups!")
-    print("20x chance of homocide")
-    print("And HIGHER CHANCES OF:\ndrug overdose\nbankruptcy\nkidnapping")
+    vote = (input("Cast your vote? (yes or no)\n"))
 
-    # Cash or time?
+    if vote == "no":
+        print("k. bye.")
 
-    money = (input("Do you want to take it all out as cash or slowly over time? (cash or time)\n"))
+    elif vote == "yes":
+        print("You have won 600 million dollars!")
+        print("You have recieved powerups!")
+        print("20x chance of homocide")
+        print("And HIGHER CHANCES OF:\ndrug overdose\nbankruptcy\nkidnapping")
 
-    if money == "time":
-        print("You just lost $$ due to annual taxes! - Bankrupt!")
-    elif money == "cash":
-        print("Good! You managed to avoid losing money")
-        print("Someone begged you to donate money")
+        # Cash or time?
 
-        # Donate or no?
+        money = (input("Do you want to take it all out as cash or slowly over time? (cash or time)\n"))
 
-        donate = (input("Will you donate? (yes or no)\n"))
+        if money == "time":
+            print("You just lost $$ due to annual taxes! - Bankrupt!")
+        elif money == "cash":
+            print("Good! You managed to avoid losing money")
+            print("Someone begged you to donate money")
 
-        if donate == "yes":
-            print("Bankrupt! After you said yes, everyone swarmed you and took your money")
-        elif donate == "no":
-            print("Great! You avoided going bankrupt due to people swarming you after the donation was given.")
+            # Donate or no?
 
-            # Ant
+            donate = (input("Will you donate? (yes or no)\n"))
 
-            Ant = (input("Your second wife has asked you for 100,000 dollars, will you give? (yes / no)\n"))
+            if donate == "yes":
+                print("Bankrupt! After you said yes, everyone swarmed you and took your money")
+            elif donate == "no":
+                print("Great! You avoided going bankrupt due to people swarming you after the donation was given.")
 
-            if Ant == "yes":
-                print("Oh no! The rest of your family is mad and one of your distant family members kills you due to jealously.")
-                print("YOU DIED")
-            elif Ant == "no":
-                print("Good job! Your second family is mad at you, but it's nothing you can't deal with.")
-                print("Rumors say that someone is out to steal your money! ")
+                # Ant
 
-                # Rumors
+                Ant = (input("Your second wife has asked you for 100,000 dollars, will you give? (yes / no)\n"))
 
-                Ru = (input("Do you want to hide your cash in your car (unlikely to find) or your closet (safer)? (car / closet)\n"))
+                if Ant == "yes":
+                    print("Oh no! The rest of your family is mad and one of your distant family members kills you due to jealously.")
+                    print("YOU DIED")
+                elif Ant == "no":
+                    print("Good job! Your second family is mad at you, but it's nothing you can't deal with.")
+                    print("Rumors say that someone is out to steal your money! ")
 
-                if Ru == "car":
-                    print("Someone stole your fortune, you lost a lot of cash. - Bankrupt!")
-                elif Ru == "closet":
-                    print("It wasn't the smartest place but... no one managed to break into your house!")
-                    print("SOMEONE IS APPROACHING...\nIt's a financial advisor and he wants you to hire him on the spot!")
+                    # Rumors
 
-                    # Financial Advisor
+                    Ru = (input("Do you want to hide your cash in your car (unlikely to find) or your closet (safer)? (car / closet)\n"))
 
-                    FA = (input("Do you want to hire him? (yes / no)\n"))
+                    if Ru == "car":
+                        print("Someone stole your fortune, you lost a lot of cash. - Bankrupt!")
+                    elif Ru == "closet":
+                        print("It wasn't the smartest place but... no one managed to break into your house!")
+                        print("SOMEONE IS APPROACHING...\nIt's a financial advisor and he wants you to hire him on the spot!")
 
-                    if FA == "yes":
-                        print("Bad decision! He has scammed you and took all of your cash!")
-                    elif FA == "no":
-                        print("Good! You avoided getting scamming from the fake financial advisor.")
+                        # Financial Advisor
 
-                        # brother sues
+                        FA = (input("Do you want to hire him? (yes / no)\n"))
 
-                        print("Your brother finds out about your second family and threatens to expose to your real family if you don't give him 2 million dollars.")
-                        brother = (input("Do you give your brother 2 million dollars? (yes / no)\n"))
+                        if FA == "yes":
+                            print("Bad decision! He has scammed you and took all of your cash!")
+                        elif FA == "no":
+                            print("Good! You avoided getting scamming from the fake financial advisor.")
 
-                        if brother == "yes":
-                            print("You make him promise not to tell anyone about the affair but he still tells your wife and she ends up murdering you, R.I.P. Homie")
-                        elif brother == "no":
-                            print("He exposes you but you change your name and move to another state therefore you're okay.")
+                            # brother sues
 
-                            #death bed
+                            print("Your brother finds out about your second family and threatens to expose to your real family if you don't give him 2 million dollars.")
+                            brother = (input("Do you give your brother 2 million dollars? (yes / no)\n"))
 
-                            print("Your first wife finds you in another state and she shanks you a couple of times and now you're in the hospital laying on your death bed.")
-                            print("Your doctor says he could save your life if you go through heart transplant surgery the only catch is the heart is from the black market.")
+                            if brother == "yes":
+                                print("You make him promise not to tell anyone about the affair but he still tells your wife and she ends up murdering you, R.I.P. Homie")
+                            elif brother == "no":
+                                print("He exposes you but you change your name and move to another state therefore you're okay.")
 
-                            heart = (input("Do you go through the surgery? (yes / no)\n"))
+                                #death bed
 
-                            if heart == "yes":
-                                print("Looks like the heart was infected and it infected the rest of your body R.I.P Homie")
-                            elif heart == "no":
-                                print("You needed that heart to live homie R.I.P")
+                                print("Your first wife finds you in another state and she shanks you a couple of times and now you're in the hospital laying on your death bed.")
+                                print("Your doctor says he could save your life if you go through heart transplant surgery the only catch is the heart is from the black market.")
+
+                                heart = (input("Do you go through the surgery? (yes / no)\n"))
+
+                                if heart == "yes":
+                                    print("Looks like the heart was infected and it infected the rest of your body R.I.P Homie")
+                                elif heart == "no":
+                                    print("You needed that heart to live homie R.I.P")
